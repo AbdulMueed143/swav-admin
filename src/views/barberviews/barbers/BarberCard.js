a7668a3cd4c3ee16983d14a35d257b118df8be51
Akash Kumar <a.kumar.17265@khi.iba.edu.pk>
2024-06-28 19:53:42 +0500

    Fixed overlapping cards bug for barbers screen

--- src/views/barberviews/barbers/BarberCard.js
+++ src/views/barberviews/barbers/BarberCard.js
@@ -1,72 +1,87 @@
-import { faCog } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  Avatar, Card } from 'components/ui'
-import Button  from 'components/ui/Buttons/Button';
-import Switcher from 'components/ui/Switcher';
-
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Avatar, Card } from 'components/ui'
+import Button from 'components/ui/Buttons/Button'
+import Switcher from 'components/ui/Switcher'
 
 const BarberCard = ({ barber, onStatusSwitcherToggle, onSettingsClicked }) => {
-
-    const { barberId, firstName, lastName, email, phoneNumber, amenities, about, status } = barber;
+    const {
+        barberId,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        amenities,
+        about,
+        status,
+    } = barber
 
     const cardFooter = (
         <div cclassName="flex flex-col items-start">
-           <div className="flex items-center mb-2">
-            <Avatar
-                size={30}
-                className="mr-2"
-                shape="circle"
-                src="https://images.unsplash.com/photo-1630827020718-3433092696e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVucyUyMGhhaXJjdXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-            />
-            <span className='truncate'>
-                <h6 className="text-sm">Services</h6>
-                <div className="text-xs overflow-hidden ">
-                    {amenities && amenities.length > 0 ? (
-                        amenities.map((amenity, index) => (
-                            <span  key={index}>{amenity.name}</span>
-                        ))
-                    ) : (
-                        <div >No services available</div>
-                    )}
-                </div>
-            </span>
-        </div>
+            <div className="flex items-center mb-2">
+                <Avatar
+                    size={30}
+                    className="mr-2"
+                    shape="circle"
+                    src="https://images.unsplash.com/photo-1630827020718-3433092696e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVucyUyMGhhaXJjdXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+                />
+                <span className="truncate">
+                    <h6 className="text-sm">Services</h6>
+                    <div className="text-xs overflow-hidden ">
+                        {amenities && amenities.length > 0 ? (
+                            amenities.map((amenity, index) => (
+                                <span key={index}>{amenity.name}</span>
+                            ))
+                        ) : (
+                            <div>No services available</div>
+                        )}
+                    </div>
+                </span>
+            </div>
 
-        <div className="flex items-center mb-2">
-
-<span>
-    <h6 className="text-sm">Detials</h6>
-    <div className="text-xs">{email}</div>
-    <div className="text-xs">{phoneNumber}</div>
-
-</span>
-</div>
+            <div className="flex items-center mb-2">
+                <span>
+                    <h6 className="text-sm">Detials</h6>
+                    <div className="text-xs">{email}</div>
+                    <div className="text-xs">{phoneNumber}</div>
+                </span>
+            </div>
 
             <div className="flex">
-
-                <Switcher  className="mr-2 mb-2" 
-                            defaultChecked={status !== "DISABLED"} 
-                            onChange={(checked) => onStatusSwitcherToggle(checked, barberId)} />
-
+                <Switcher
+                    className="mr-2 mb-2"
+                    defaultChecked={status !== 'DISABLED'}
+                    onChange={(checked) =>
+                        onStatusSwitcherToggle(checked, barberId)
+                    }
+                />
             </div>
         </div>
     )
 
-
-
     const cardHeader = (
-        <div className="rounded-tl-lg rounded-tr-lg overflow-hidden relative" style={{ height: '250px', width: '300px' }}>
+        <div
+            className="rounded-tl-lg rounded-tr-lg overflow-hidden relative"
+            style={{ height: '250px', width: '300px' }}
+        >
             <img
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                 src="https://images.unsplash.com/photo-1630827020718-3433092696e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVucyUyMGhhaXJjdXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
                 alt="card header"
             />
-            <FontAwesomeIcon icon={faCog} className="absolute top-0 right-0 m-2 text-white" onClick={() => onSettingsClicked(barberId) } />
+            <FontAwesomeIcon
+                icon={faCog}
+                className="absolute top-0 right-0 m-2 text-white"
+                onClick={() => onSettingsClicked(barberId)}
+            />
         </div>
-    );
-    
+    )
+
     return (
-        <div className="max-w-xs" style={{ width: '300px', height: '500px' }}>
+        <div
+            className="max-w-xs"
+            style={{ width: '300px', minHeight: '500px' }}
+        >
             <Card
                 clickable
                 className="hover:shadow-lg transition duration-150 ease-in-out dark:border dark:border-gray-600 dark:border-solid"
@@ -79,14 +94,13 @@ const BarberCard = ({ barber, onStatusSwitcherToggle, onSettingsClicked }) => {
                 {/* <span className="text-emerald-600 font-semibold">
                     Cost 60 AUD
                 </span> */}
-                <h4 className="font-bold my-3">{firstName} {lastName}</h4>
-                <p className="truncate">
-                    {about}             
-                </p>
+                <h4 className="font-bold my-3">
+                    {firstName} {lastName}
+                </h4>
+                <p /*className="truncate"*/>{about}</p>
             </Card>
         </div>
     )
 }
 
 export default BarberCard
-
--- src/views/barberviews/barbers/BarbersGrid.js
+++ src/views/barberviews/barbers/BarbersGrid.js
@@ -1,166 +1,168 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import React from 'react'
-import BarberCard from './BarberCard';
+import BarberCard from './BarberCard'
 import ButtonWithIcon from '../../../components/ui/custom/barbers/ButtonWithIcon'
-import AddBarberModal from './dialogs/AddBarberModal';
+import AddBarberModal from './dialogs/AddBarberModal'
 import useBookingServices from 'utils/hooks/useBookingService'
-import { Loading } from 'components/shared';
-import Button  from 'components/ui/Buttons/Button';
-import { Dialog } from 'components/ui';
+import { Loading } from 'components/shared'
+import Button from 'components/ui/Buttons/Button'
+import { Dialog } from 'components/ui'
 import { useSelector } from 'react-redux'
-
 
 const BarbersGrid = () => {
     // Initialize state for the search input
-    const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const userInfo = useSelector((state) => state.auth.user);
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const userInfo = useSelector((state) => state.auth.user)
 
-    const { getBarbers, addBarbers, disableBarber } = useBookingServices();
-    const [barbers, setBarbers] = useState([]); // Initial state as an empty array
-    const [search, setSearch] = useState('');
+    const { getBarbers, addBarbers, disableBarber } = useBookingServices()
+    const [barbers, setBarbers] = useState([]) // Initial state as an empty array
+    const [search, setSearch] = useState('')
 
     //Handling the Disable click ...
     const [disableDialogIsOpen, setIsDisableDialogOpen] = useState(false)
-    const [selectedId, setSelectedId] = useState(null);
-    const [selectedStatus, setSelectedStatus] = useState(null);
+    const [selectedId, setSelectedId] = useState(null)
+    const [selectedStatus, setSelectedStatus] = useState(null)
 
-
-    const onBarberSettingClicked = (barberId) => {
-
-    }
+    const onBarberSettingClicked = (barberId) => {}
 
     const onStatusSwitcherToggle = (checked, id) => {
-        setSelectedId(id);
-        setSelectedStatus(checked);
-        setIsDisableDialogOpen(true);
+        setSelectedId(id)
+        setSelectedStatus(checked)
+        setIsDisableDialogOpen(true)
     }
 
-    const onDisableDialogClose =() => {
-        setSelectedId(null);
-        setSelectedStatus(null);
-        setIsDisableDialogOpen(false);
+    const onDisableDialogClose = () => {
+        setSelectedId(null)
+        setSelectedStatus(null)
+        setIsDisableDialogOpen(false)
     }
 
     const onDisableDialogOk = async () => {
-        setIsDisableDialogOpen(false);
-        setLoading(true);
+        setIsDisableDialogOpen(false)
+        setLoading(true)
 
-        const data = await disableBarber(selectedId, selectedStatus);
-        if(data.status === -1) {
+        const data = await disableBarber(selectedId, selectedStatus)
+        if (data.status === -1) {
             //something went wrong ...
-            setLoading(false);
+            setLoading(false)
+        } else {
+            fetchBarbers()
         }
-        else {
-              fetchBarbers();
-        }
-
     }
 
     const fetchBarbers = async () => {
-        setLoading(true);
-        const data = await getBarbers();
-        setBarbers(data);
-        setLoading(false);
-    };
-    
+        setLoading(true)
+        const data = await getBarbers()
+        setBarbers(data)
+        setLoading(false)
+    }
+
     useEffect(() => {
-        fetchBarbers();
-    }, []); // Empty dependency array means the effect will only run once
+        fetchBarbers()
+    }, []) // Empty dependency array means the effect will only run once
 
- 
     const handleClickToOpen = () => {
-        setOpen(true);
-    };
- 
-    const handleToClose = () => {
-        setOpen(false);
-    };
+        setOpen(true)
+    }
 
+    const handleToClose = () => {
+        setOpen(false)
+    }
 
     const handleBarberSave = async (formValues, amenities) => {
         // Handle the form submission here using formValues
         formValues.properties = {}
-        formValues.amenities = amenities;
+        formValues.amenities = amenities
 
         //lets make call to server
-        const data = await addBarbers(formValues);
-        if(data.status === -1) {
+        const data = await addBarbers(formValues)
+        if (data.status === -1) {
             //something went wrong ...
+        } else {
+            // Call fetchServices to refresh the services
+            fetchBarbers()
         }
-        else {
-              // Call fetchServices to refresh the services
-            fetchBarbers();
-        }
 
-        setOpen(false);
-    };
-
-
+        setOpen(false)
+    }
 
     // Filter the services based on the search input
-    const filteredBarbers = barbers.filter(barber => 
+    const filteredBarbers = barbers.filter((barber) =>
         barber.firstName.toLowerCase().includes(search.toLowerCase())
-    );
+    )
 
     return (
-        <div className="w-full"> 
-
-        <div className="flex justify-end gap-4 items-center">
+        <div className="w-full">
+            <div className="flex justify-end gap-4 items-center">
                 <input
                     type="text"
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                     placeholder="Search for a barber..."
                     className="p-2 border rounded flex-grow"
+                />
+
+                {userInfo &&
+                Array.isArray(userInfo.roles) &&
+                userInfo.roles.length > 0 &&
+                userInfo.roles[0] == 'OWNER' ? (
+                    <ButtonWithIcon
+                        label="Add Barber"
+                        onClick={handleClickToOpen}
                     />
-
-                        {(userInfo && Array.isArray(userInfo.roles) && userInfo.roles.length > 0 && (userInfo.roles[0] == 'OWNER')) ? 
-                        (
-                            <ButtonWithIcon 
-                                label="Add Barber"  onClick={handleClickToOpen} 
-                            />
-                        ) : 
-                        (
-                            <div></div>
-                        )}
-
-                </div>
-
-                <Dialog
-                    isOpen={disableDialogIsOpen}
-                    onClose={onDisableDialogClose}
-                    onRequestClose={onDisableDialogClose}
-                >
-                    <h5 className="mb-4">{selectedStatus ? "Enable" : "Disable"} Barber</h5>
-                    <p>
-                        Are you sure you want to {selectedStatus ? "Enable" : "Disable"} this Barber?
-                    </p>
-                    <div className="text-right mt-6">
-                        <Button
-                            className="ltr:mr-2 rtl:ml-2"
-                            variant="plain"
-                            onClick={onDisableDialogClose}
-                        >
-                            Cancel
-                        </Button>
-                        <Button variant="solid" onClick={onDisableDialogOk}>
-                            Okay
-                        </Button>
-                    </div>
-                </Dialog>
-
-                <Loading loading={loading} >
-                    <div className="flex gap-4 flex-wrap mt-4"> 
-                        {filteredBarbers.map((barber, index) => (
-                            <BarberCard key={index} barber={barber} onSettingsClicked={onBarberSettingClicked} onStatusSwitcherToggle={onStatusSwitcherToggle} />
-                        ))}
-                    </div>
-                </Loading>
-
-                <AddBarberModal open={open} handleToSave={handleBarberSave} handleToClose={handleToClose} />
+                ) : (
+                    <div></div>
+                )}
             </div>
-    );
-};
 
-export default BarbersGrid;
+            <Dialog
+                isOpen={disableDialogIsOpen}
+                onClose={onDisableDialogClose}
+                onRequestClose={onDisableDialogClose}
+            >
+                <h5 className="mb-4">
+                    {selectedStatus ? 'Enable' : 'Disable'} Barber
+                </h5>
+                <p>
+                    Are you sure you want to{' '}
+                    {selectedStatus ? 'Enable' : 'Disable'} this Barber?
+                </p>
+                <div className="text-right mt-6">
+                    <Button
+                        className="ltr:mr-2 rtl:ml-2"
+                        variant="plain"
+                        onClick={onDisableDialogClose}
+                    >
+                        Cancel
+                    </Button>
+                    <Button variant="solid" onClick={onDisableDialogOk}>
+                        Okay
+                    </Button>
+                </div>
+            </Dialog>
+
+            <Loading loading={loading}>
+                <div className="flex gap-4 flex-wrap mt-4">
+                    {filteredBarbers.map((barber, index) => (
+                        <BarberCard
+                            key={index}
+                            barber={barber}
+                            onSettingsClicked={onBarberSettingClicked}
+                            onStatusSwitcherToggle={onStatusSwitcherToggle}
+                            className="flex"
+                        />
+                    ))}
+                </div>
+            </Loading>
+
+            <AddBarberModal
+                open={open}
+                handleToSave={handleBarberSave}
+                handleToClose={handleToClose}
+            />
+        </div>
+    )
+}
+
+export default BarbersGrid
